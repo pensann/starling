e0cb75608f6cc835707c988396acba9239a1bdd8
songao <eno.cat@live.com>
2021-08-19 12:22:19 +0800

    初步实现遍历器，但因Load不支持直接的i18n需要重构部分代码

---
+++ tests/backup/src-i18n-backup/interfaces/results.ts
@@ -0,0 +1,16 @@
+/** 最基础的字符串K-V 字典 */
+interface DictKV {
+    [i: string]: string
+}
+
+/** 包含修改后的Change和包含的字符串字典 */
+interface ChangeTravResult {
+    alter: CommonFields, // 修改后的CommonChange对象
+    dict: DictKV, // 提取的字符串字典
+}
+
+/** 包含修改后的Entries和字符串字典 */
+interface EntriesTravResult {
+    alter: Entries,
+    dict: DictKV // 提取的字符串字典
+}
